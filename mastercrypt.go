68fd7932b73e082b1f68fd76eb33879f231dc6cf
tmbracuti <tmbracuti@yahoo.com>
2021-07-29 11:36:01 -0400

    update

--- mastercrypt.go
+++ mastercrypt.go
@@ -10,47 +10,6 @@ import (
 	"io"
 	"os"
 )
-/*
-func ExampleNewCFBDecrypter(cipher_in string) string {
-	
-	key := []byte{0xaa, 0xde, 0xff, 0xfa, 0xaa, 0xde, 0xff, 0xfa,0xa0, 0xd0, 0xff, 0xc1,0xca, 0xde, 0x07, 0x01}  	
-	ciphertext, err := hex.DecodeString(cipher_in)
-	for _, b := range ciphertext {
-		fmt.Printf("%x ", b)
-	}
-	fmt.Printf("\n")
-
-	if err != nil {
-		fmt.Println(err);
-	}
-  	block, err := aes.NewCipher(key)
-  	if err != nil {
-  		panic(err)
-  	}
-
-  	if len(ciphertext) < aes.BlockSize {
-  		panic("ciphertext too short")
-  	}
-  	
-  	iv := ciphertext[:aes.BlockSize]
-	for _, b := range iv {
-		fmt.Printf("%x ", b)
-	}
-	fmt.Printf("\n")
-	
-	ciphertext = ciphertext[aes.BlockSize:]
-	for _, b := range ciphertext {
-		fmt.Printf("%x ", b)
-	}
-	fmt.Printf("\n")
-  
-  	stream := cipher.NewCFBDecrypter(block, iv)
-    	
-  	stream.XORKeyStream(ciphertext, ciphertext)
-  	ret := fmt.Sprintf("%s", ciphertext)
-  	return ret
-}
-*/
 
 func StarfishCFBEncrypter(plainTxt string) string {
   	
@@ -71,14 +30,12 @@ func StarfishCFBEncrypter(plainTxt string) string {
   
   	stream := cipher.NewCFBEncrypter(block, iv)
   	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
-	txt := fmt.Sprintf("%x", ciphertext)	
-	//fmt.Printf("ciphertext bytes: %x\n", ciphertext)
+	txt := fmt.Sprintf("%x", ciphertext)
 	return txt
   
 }
 
-func main() { 
-	//fmt.Printf("aes block size: %d\n", aes.BlockSize)
+func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("usage: ./mastercrypt <password>")
 		return
